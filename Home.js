090201a80557a9fd7a2e029b8ace2d8503b96ea5
prithvishetty925 <122429412+prithvishetty925@users.noreply.github.com>
2023-07-24 17:24:07 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+        <h1>Wanderlust</h1>
+        
+        <h3>Our Tours</h3>
+        <p>Mumbai</p>
+        <p>Kerala</p>
+        <p>Karnataka</p>
+        <p>Goa</p>
+        <p>Tamil Nadu</p>
+        <p>Uttar Pradesh</p>
+        </>
+    );
+};
+export default Home;
